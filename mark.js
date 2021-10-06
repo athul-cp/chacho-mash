d249871d3d721ccd7666defbc33b2c4bb5ba81d6
shamil ollakkan <shamilollakkan@gmail.com>
2021-10-07 04:34:30 +0530

    ec syllabus added

--- mark.js
+++ mark.js
@@ -95,52 +95,271 @@ let ec22015 = [
 ]
 
 
-let ec22015 = [
+let ec32015 = [
     {
-        code : "AS 15-1201B",
-        sub_name : "Calculus",
+        code : "AS 15-1301",
+        sub_name : "linear Algebra & Transform Techniques",
         credit : 3
     },
     {
-        code : "AS 15-1202B",
-        sub_name : "Engineering Physics",
+        code : "EC 15-1302",
+        sub_name : "Discrete Computational structures",
         credit : 3
 
     },
     {
-        code : "GE 15-1203B",
-        sub_name : "Engineering Mechanics",
-        credit : 4
+        code : "EC 15-1303",
+        sub_name : "Network Theory",
+        credit : 3
 
     },
     {
-        code : "GE 15-1205B",
-        sub_name : "Basic Mechanical Engineering",
+        code : "EC 15-1304",
+        sub_name : "Digital Electronics",
         credit : 3
     },
     {
-        code : "HS 15-12L1B",
-        sub_name : "Technical Communications and Professional Ethics",
+        code : "EC 15-1305",
+        sub_name : "Solid State Electronics",
+        credit : 3
+    },
+    {
+        code : "EC 15-1306",
+        sub_name : "Electronic Circuits I",
+        credit : 3
+    },
+    {
+        code : "EC 15-13L1",
+        sub_name : "Basic Electronics Lab",
         credit : 2
     },
     {
-        code : "GE 15-12L1B",
-        sub_name : "Civil Engineering Workshop",
+        code : "Ec 15-13L2",
+        sub_name : "Digital Electronics Lab",
+        credit : 2
+    }
+    
+]
+let ec42015 = [
+    {
+        code : "AS 15-1401",
+        sub_name : "Complex Variables and Partial Differential Equations",
+        credit : 3
+    },
+    {
+        code : "EC 15-1402",
+        sub_name : "Microprocessor Architectures & Programming",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1403",
+        sub_name : "Electronic Circuits II",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1404",
+        sub_name : "Signals & Systems",
+        credit : 3
+    },
+    {
+        code : "EC 15-1405",
+        sub_name : "Communication Engineering I",
+        credit : 3
+    },
+    {
+        code : "EC 15-1406",
+        sub_name : "Digital System Design",
+        credit : 3
+    },
+    {
+        code : "EC 15-14L1",
+        sub_name : "Digital Programming Lab",
+        credit : 2
+    },
+    {
+        code : "EC 15-14L2",
+        sub_name : "Electronic Circuits Lab I",
+        credit : 2
+    }
+]
+
+
+let ec52015 = [
+    {
+        code : "AS 15-1501",
+        sub_name : "Numerical and Statistical Methods",
+        credit : 3
+    },
+    {
+        code : "EC 15-1502",
+        sub_name : "Electromagnetic Theory",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1503",
+        sub_name : "Embedded Systems",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1504",
+        sub_name : "Communication Engineering II",
+        credit : 3
+    },
+    {
+        code : "EC 15-1505",
+        sub_name : "Analog & Integrated Circuits",
+        credit : 3
+    },
+    {
+        code : "EC 15-1506",
+        sub_name : "Digital Signal Processing",
+        credit : 3
+    },
+    {
+        code : "EC 15-15L1",
+        sub_name : "Mini Project",
+        credit : 2
+    },
+    {
+        code : "EC 15-15L2",
+        sub_name : "Electronic Circuits Lab II",
+        credit : 2
+    }
+]
+let ec62015 = [
+    {
+        code : "EC 15-1601",
+        sub_name : "Electronic Measurements & Instrumentation",
+        credit : 3
+    },
+    {
+        code : "EC 15-1602",
+        sub_name : "Microwave Techniques & Devices",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1603",
+        sub_name : "VLSI Design",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1604",
+        sub_name : "Information Theory & Coding",
+        credit : 3
+    },
+    {
+        code : "EC 15-1605",
+        sub_name : "Power Electronics",
+        credit : 3
+    },
+    {
+        code : "EC 15-1606",
+        sub_name : "Elective – I",
+        credit : 3
+    {
+        code : "EC 15-16L1",
+        sub_name : "Digital Signal Processing Lab",
+        credit : 2
+    },
+    {
+        code : "EC 15-16L2",
+        sub_name : "Communication Lab I",
+        credit : 2
+    }
+
+
+let ec72015 = [
+    {
+        code : "GE 15-1701",
+        sub_name : "Principles of Management",
+        credit : 3
+    },
+    {
+        code : "EC 15-1702",
+        sub_name : "Antennas & Propagation",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1703",
+        sub_name : "Digital Image Processing",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1704",
+        sub_name : "Control Systems Engineering",
+        credit : 3
+    },
+    {
+        code : "EC 15-1705",
+        sub_name : "Elective II",
+        credit : 3
+    },
+    {
+        code : "EC 15-17L1",
+        sub_name : "Embedded Systems Lab",
+        credit : 2
+    },
+    {
+        code : "EC 15-17L2",
+        sub_name : "Communication Lab II",
+        credit : 2
+    },
+    {
+        code : "GE 15-17L3",
+        sub_name : "Entrepreneurship Development",
         credit : 1
     },
     {
-        code : "GE 15-12L2B",
-        sub_name : "Mechanical Engineeering Workshop",
-        credit : 1
+        code : "EC 15-17L4",
+        sub_name : "Project Phase I & Industrial Internship",
+        credit : 2
+    }
+]
+
+let ec82015 = [
+    {
+        code : "EC 15-1801",
+        sub_name : "Electronic Product Design",
+        credit : 3
     },
     {
-        code : "HS 15-12L3B",
-        sub_name : "Language Lab",
-        credit : 1
+        code : "EC 15-1802",
+        sub_name : "Wireless Communication",
+        credit : 3
+
     },
     {
-        code : "HS 15-12L4B",
-        sub_name : "NSS/Nature Conversation",
-        credit : 1
+        code : "EC 15-1803",
+        sub_name : "Computer Communication & Networking",
+        credit : 3
+
+    },
+    {
+        code : "EC 15-1804",
+        sub_name : "Elective III",
+        credit : 3
+    },
+    {
+        code : "EC 15-18L1",
+        sub_name : "Seminar",
+        credit : 2
+    },
+    {
+        code : "EC 15-18L2",
+        sub_name : "Project Phase 2",
+        credit : 6
+    },
+    {
+        code : "EC 15-18L3",
+        sub_name : "Comprehensive Viva Voce",
+        credit : 2
     }
 ]
