446e07879871587499986fce82fe88a2b397ea0c
shamil ollakkan <shamilollakkan@gmail.com>
2021-10-07 03:35:20 +0530

    ec1 syllabus added

---
+++ mark.js
@@ -0,0 +1,44 @@
+let ec12015 = [
+    {
+        code : "GE 15-1101B",
+        sub_name : "computer programming",
+        credit : 3
+    },
+    {
+        code : "AS 15-1102B",
+        sub_name : "engineering chemistry",
+        credit : 3
+
+    },
+    {
+        code : "GE 15-1103B",
+        sub_name : "engineering graphics",
+        credit : 5
+
+    },
+    {
+        code : "GE 15-1104B",
+        sub_name : "basic electrical engineering",
+        credit : 3
+    },
+    {
+        code : "GE 15-1105B",
+        sub_name : "basic electronics engineering",
+        credit : 3
+    },
+    {
+        code : "AS 15-1106B",
+        sub_name : "environmental studies",
+        credit : 3
+    },
+    {
+        code : "GE 15-11L1B",
+        sub_name : "electtrical engineeering workshop",
+        credit : 1
+    },
+    {
+        code : "GE 15-11L2B",
+        sub_name : "computer programmiing laboratory",
+        credit : 1
+    }
+]
