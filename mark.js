6557b8883b02c66fc3cd923a19655dd75fb8f2bd
shamil ollakkan <shamilollakkan@gmail.com>
2021-10-07 03:48:25 +0530

    ec2 syllabus added

--- mark.js
+++ mark.js
@@ -1,44 +1,146 @@
 let ec12015 = [
     {
         code : "GE 15-1101B",
-        sub_name : "computer programming",
+        sub_name : "Computer Programming",
         credit : 3
     },
     {
         code : "AS 15-1102B",
-        sub_name : "engineering chemistry",
+        sub_name : "Engineering Chemistry",
         credit : 3
 
     },
     {
         code : "GE 15-1103B",
-        sub_name : "engineering graphics",
+        sub_name : "Engineering Graphics",
         credit : 5
 
     },
     {
         code : "GE 15-1104B",
-        sub_name : "basic electrical engineering",
+        sub_name : "Basic Electrical Engineering",
         credit : 3
     },
     {
         code : "GE 15-1105B",
-        sub_name : "basic electronics engineering",
+        sub_name : "Basic Electronics Engineering",
         credit : 3
     },
     {
         code : "AS 15-1106B",
-        sub_name : "environmental studies",
+        sub_name : "Environmental Studies",
         credit : 3
     },
     {
         code : "GE 15-11L1B",
-        sub_name : "electtrical engineeering workshop",
+        sub_name : "Electtrical Engineeering Workshop",
         credit : 1
     },
     {
         code : "GE 15-11L2B",
-        sub_name : "computer programmiing laboratory",
+        sub_name : "Computer Programmiing Laboratory",
+        credit : 1
+    }
+]
+
+
+let ec22015 = [
+    {
+        code : "AS 15-1201B",
+        sub_name : "Calculus",
+        credit : 3
+    },
+    {
+        code : "AS 15-1202B",
+        sub_name : "Engineering Physics",
+        credit : 3
+
+    },
+    {
+        code : "GE 15-1203B",
+        sub_name : "Engineering Mechanics",
+        credit : 4
+
+    },
+    {
+        code : "GE 15-1205B",
+        sub_name : "Basic Mechanical Engineering",
+        credit : 3
+    },
+    {
+        code : "HS 15-12L1B",
+        sub_name : "Technical Communications and Professional Ethics",
+        credit : 2
+    },
+    {
+        code : "GE 15-12L1B",
+        sub_name : "Civil Engineering Workshop",
+        credit : 1
+    },
+    {
+        code : "GE 15-12L2B",
+        sub_name : "Mechanical Engineeering Workshop",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L3B",
+        sub_name : "Language Lab",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L4B",
+        sub_name : "NSS/Nature Conversation",
+        credit : 1
+    }
+]
+
+
+let ec22015 = [
+    {
+        code : "AS 15-1201B",
+        sub_name : "Calculus",
+        credit : 3
+    },
+    {
+        code : "AS 15-1202B",
+        sub_name : "Engineering Physics",
+        credit : 3
+
+    },
+    {
+        code : "GE 15-1203B",
+        sub_name : "Engineering Mechanics",
+        credit : 4
+
+    },
+    {
+        code : "GE 15-1205B",
+        sub_name : "Basic Mechanical Engineering",
+        credit : 3
+    },
+    {
+        code : "HS 15-12L1B",
+        sub_name : "Technical Communications and Professional Ethics",
+        credit : 2
+    },
+    {
+        code : "GE 15-12L1B",
+        sub_name : "Civil Engineering Workshop",
+        credit : 1
+    },
+    {
+        code : "GE 15-12L2B",
+        sub_name : "Mechanical Engineeering Workshop",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L3B",
+        sub_name : "Language Lab",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L4B",
+        sub_name : "NSS/Nature Conversation",
         credit : 1
     }
 ]
