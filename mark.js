b59c7316b657109098af369d8b6a1d26f9042d27
shamil ollakkan <shamilollakkan@gmail.com>
2021-10-07 08:48:22 +0530

    cs marks

--- mark.js
+++ mark.js
@@ -364,3 +364,381 @@ let ec82015 = [
         credit : 2
     }
 ]
+let cs12015 = [
+    {
+        code : "GE 15-1101B",
+        sub_name : "Computer Programming",
+        credit : 3
+    },
+    {
+        code : "AS 15-1102B",
+        sub_name : "Engineering Chemistry",
+        credit : 3
+
+    },
+    {
+        code : "GE 15-1103B",
+        sub_name : "Engineering Graphics",
+        credit : 5
+
+    },
+    {
+        code : "GE 15-1104B",
+        sub_name : "Basic Electrical Engineering",
+        credit : 3
+    },
+    {
+        code : "GE 15-1105B",
+        sub_name : "Basic Electronics Engineering",
+        credit : 3
+    },
+    {
+        code : "AS 15-1106B",
+        sub_name : "Environmental Studies",
+        credit : 3
+    },
+    {
+        code : "GE 15-11L1B",
+        sub_name : "Electtrical Engineeering Workshop",
+        credit : 1
+    },
+    {
+        code : "GE 15-11L2B",
+        sub_name : "Computer Programmiing Laboratory",
+        credit : 1
+    }
+]
+
+
+let cs22015 = [
+    {
+        code : "AS 15-1201B",
+        sub_name : "Calculus",
+        credit : 3
+    },
+    {
+        code : "AS 15-1202B",
+        sub_name : "Engineering Physics",
+        credit : 3
+
+    },
+    {
+        code : "GE 15-1203B",
+        sub_name : "Engineering Mechanics",
+        credit : 4
+
+    },
+    {
+        code : "GE 15-1205B",
+        sub_name : "Basic Mechanical Engineering",
+        credit : 3
+    },
+    {
+        code : "HS 15-12L1B",
+        sub_name : "Technical Communications and Professional Ethics",
+        credit : 2
+    },
+    {
+        code : "GE 15-12L1B",
+        sub_name : "Civil Engineering Workshop",
+        credit : 1
+    },
+    {
+        code : "GE 15-12L2B",
+        sub_name : "Mechanical Engineeering Workshop",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L3B",
+        sub_name : "Language Lab",
+        credit : 1
+    },
+    {
+        code : "HS 15-12L4B",
+        sub_name : "NSS/Nature Conversation",
+        credit : 1
+    }
+]
+
+
+let cs32015 = [
+    {
+        code : "AS15-1301",
+        sub_name : "Linear Algebra and Transform Techniques",
+        credit : 3
+    },
+    {
+        code : "CS15-1302",
+        sub_name : "Logic Design",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1303",
+        sub_name : "Discrete Computational Structures",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1304",
+        sub_name : "Object Oriented Programming",
+        credit : 3
+    },
+    {
+        code : "CS15-1305",
+        sub_name : "Principles of Programming Languages",
+        credit : 3
+    },
+    {
+        code : "CS15-1306",
+        sub_name : "Data and Computer Communication",
+        credit : 3
+    },
+    {
+        code : "CS15-13L1",
+        sub_name : "Digital Electronics Laboratory",
+        credit : 2
+    },
+    {
+        code : "CS15-13L2",
+        sub_name : "Object Oriented Programming Laboratory",
+        credit : 2
+    }
+]
+
+
+let cs42015 = [
+    {
+        code : "AS15-1401",
+        sub_name : "Complex Variables and Partial Differential Equations",
+        credit : 3
+    },
+    {
+        code : "CS15-1402",
+        sub_name : "Microprocessors",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1403",
+        sub_name : "Computer Architecture and Organization",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1404",
+        sub_name : "Automata Languages and Computations",
+        credit : 3
+    },
+    {
+        code : "CS15-1405",
+        sub_name : "Data Structures and Algorithms",
+        credit : 3
+    },
+    {
+        code : "CS15-1406",
+        sub_name : "Database Management Systems",
+        credit : 3
+    },
+    {
+        code : "CS15-14L1",
+        sub_name : "Database Management Systems Laboratory",
+        credit : 2
+    },
+    {
+        code : "CS15-14L2",
+        sub_name : "Data Structures Laboratory",
+        credit : 2
+    }
+]
+
+
+
+
+let cs52015 = [
+    {
+        code : "AS15-1501",
+        sub_name : "Numerical and Statistical Methods",
+        credit : 3
+    },
+    {
+        code : "CS15-1502",
+        sub_name : "System Programming",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1503",
+        sub_name : "Object Oriented Software Engineering",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1504",
+        sub_name : "Operating System",
+        credit : 3
+    },
+    {
+        code : "CS15-1505",
+        sub_name : "Advanced Microprocessors and Microcontrollers",
+        credit : 3
+    },
+    {
+        code : "CS15-1506",
+        sub_name : "Computer Graphics",
+        credit : 3
+    },
+    {
+        code : "CS15-15L1",
+        sub_name : "Computer Graphics Laboratory",
+        credit : 2
+    },
+    {
+        code : "CS15-15L2",
+        sub_name : "Microprocessors Laboratory",
+        credit : 2
+    }
+]
+
+
+
+let cs62015 = [
+    {
+        code : "CS15-1601",
+        sub_name : "Computer Networks",
+        credit : 3
+    },
+    {
+        code : "CS15-1602",
+        sub_name : "Compiler Construction",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1603",
+        sub_name : "Cryptography and Network Security",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1604",
+        sub_name : "Data Mining",
+        credit : 3
+    },
+    {
+        code : "CS15-1605",
+        sub_name : "Embedded System Design",
+        credit : 3
+    },
+    {
+        code : "CS15-1606",
+        sub_name : "Elective I",
+        credit : 3
+    },
+    {
+        code : "CS15-16L1",
+        sub_name : "Operating System Laboratory",
+        credit : 2
+    },
+    {
+        code : "CS15-16L2",
+        sub_name : "Mini Project",
+        credit : 2
+    }
+]
+
+
+
+let cs72015 = [
+    {
+        code : "GE15-1701",
+        sub_name : "Principles of Management",
+        credit : 3
+    },
+    {
+        code : "CS15-1702",
+        sub_name : "Advanced Computer Networks",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1703",
+        sub_name : "Analysis and Design of Algorithms",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1704",
+        sub_name : "Web Technologies",
+        credit : 3
+    },
+    {
+        code : "CS15-1705",
+        sub_name : "Elective II",
+        credit : 3
+    },
+    {
+        code : "CS15-17L1",
+        sub_name : "Language Processors Laboratory",
+        credit : 2
+    },
+    {
+        code : "CS15-17L2",
+        sub_name : "Networks Laboratory",
+        credit : 2
+    },
+    {
+        code : "GE15-17L3",
+        sub_name : "Entrepreneurship Development",
+        credit : 1
+    },
+    {
+        code : "CS15-17L4",
+        sub_name : "Project Phase I and Industrial Internship",
+        credit : 2
+
+    }
+]
+
+
+
+let cs82015 = [
+    {
+        code : "CS15-1801",
+        sub_name : "Advanced Architecture and
+        Parallel Processing",
+        credit : 3
+    },
+    {
+        code : "CS15-1802",
+        sub_name : "Artificial Intelligence",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1803",
+        sub_name : "Distributed Computing",
+        credit : 3
+
+    },
+    {
+        code : "CS15-1804",
+        sub_name : "Elective III",
+        credit : 3
+    },
+    {
+        code : "CS15-18L1",
+        sub_name : "Seminar",
+        credit : 3
+    },
+    {
+        code : "CS15-18L2",
+        sub_name : "Project Phase II",
+        credit : 3
+    },
+    {
+        code : "CS15-18L3",
+        sub_name : "Comprehensive Viva Voce",
+        credit : 2
+    }
+]
